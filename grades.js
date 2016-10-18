f29de62fca0e38cda0f82b4122b1cf57ece347bb
Paul Martin <Dmart331@gmail.com>
2016-10-18 18:37:06 -0500

    Placing in correct array now TODO console.log LOW n HIGH

--- grades.js
+++ grades.js
@@ -5,35 +5,55 @@ var scoreB = [];
 var scoreC = [];
 var scoreD = [];
 var scoreF = [];
+var lowScore = [];
+var highScore = [];
 
 for(i=0; i<scores.length; i++){
 
-	if(scores[i] < 60) {
-		console.log(scoreF)
-		scores = scoreF.push("F");
+	if(scores[i] <= 60) {
+		scoreF.push(i);
+			if(scores[i] < 56){
+				lowScore.push(i);
+			}
 	}
 		else if (scores[i] <= 70 && scores[i] >= 61){
-			scoreD.push("D");
+			scoreD.push(i);
 		}
 		else if (scores[i] <= 80 && scores[i] >= 71){
-			scoreC.push("C");
+			scoreC.push(i);
 		}
 		else if (scores[i] <= 90 && scores[i] >= 81){
-			scoreB.push("B");
+			scoreB.push(i);
 		}
-		else (scores[i] > 91) 
-			scoreA.push("A");		
-}
+		else  if(scores[i] > 91){
+
+			scoreA.push(i);		
+		}
+
+		else if (scores[i] > 56){
+			lowScore.push(i);
+		}
+
+		else if(scores[i] > 95){
+			highScore.push(i)
+		}
+
+	}
 var uno = "How many As were there-  "
 var dos = "How many Bs were there-  "
 var tres = "How many Cs were there-  "
 var quatro = "How many Ds were there-  "
 var diaz = "How many Fs were there-  "
+var low = "Lowest score"
+var high = "Highest score"
+
 console.log(uno + scoreA.length);
 console.log(dos + scoreB.length);
 console.log(tres + scoreC.length);
 console.log(quatro + scoreD.length);
 console.log(diaz + scoreF.length);
+console.log("Low Score: "lowScore);
+console.log(highScore)
 
 // A score of 50-60 is an F
 // A score of 61-70 is a D
