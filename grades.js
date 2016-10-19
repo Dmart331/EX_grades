0e4fc967832dab42e0dd315f1b409519c6ad53df
Paul Martin <Dmart331@gmail.com>
2016-10-18 19:24:16 -0500

    Finished ex, now shows lows and highs

--- grades.js
+++ grades.js
@@ -13,7 +13,7 @@ for(i=0; i<scores.length; i++){
 	if(scores[i] <= 60) {
 		scoreF.push(i);
 			if(scores[i] < 56){
-				lowScore.push(i);
+				lowScore.push(scores[i]);
 			}
 	}
 		else if (scores[i] <= 70 && scores[i] >= 61){
@@ -25,17 +25,12 @@ for(i=0; i<scores.length; i++){
 		else if (scores[i] <= 90 && scores[i] >= 81){
 			scoreB.push(i);
 		}
-		else  if(scores[i] > 91){
+		else  if(scores[i] >= 91){
 
 			scoreA.push(i);		
-		}
-
-		else if (scores[i] > 56){
-			lowScore.push(i);
-		}
-
-		else if(scores[i] > 95){
-			highScore.push(i)
+				if(scores[i] > 97){
+					highScore.push(scores[i]);
+			}
 		}
 
 	}
@@ -44,16 +39,14 @@ var dos = "How many Bs were there-  "
 var tres = "How many Cs were there-  "
 var quatro = "How many Ds were there-  "
 var diaz = "How many Fs were there-  "
-var low = "Lowest score"
-var high = "Highest score"
 
 console.log(uno + scoreA.length);
 console.log(dos + scoreB.length);
 console.log(tres + scoreC.length);
 console.log(quatro + scoreD.length);
 console.log(diaz + scoreF.length);
-console.log("Low Score: "lowScore);
-console.log(highScore)
+console.log("Low Score: " + lowScore);
+console.log("High Score: " + highScore);
 
 // A score of 50-60 is an F
 // A score of 61-70 is a D
